3e3114c6f1390ce3985d3e91b8463eb0401b872d
plaidpizazz <marcellus.spears@gmail.com>
2017-03-14 07:14:38 -0500

    Added second fizzbuzz function

--- checkpoint14_fizzbuzz.js
+++ checkpoint14_fizzbuzz.js
@@ -10,15 +10,19 @@ var fizzBuzzCustom = function(stringOne, stringTwo, numOne, numTwo) {
 };
 fizzBuzzCustom("Fizz", "Buzz", 3, 5);
 
-function transposeTwoStrings(matrix) {
-  let m = [];
-  for (let j = 0; j < matrix[0].length; j++)
-  {
-    let temp = [];
-    for (let i = 0; i < matrix.length; i++)
-      temp.push(matrix"[i]");
-    m.push(temp);
-  }
-  return m;
-}
-transposeTwoStrings(["Hello", "World"]);
+var fizzBuzzCustom = function(stringOne, stringTwo, numOne, numTwo) {
+  var i;
+  var output;
+  for (i = 1; i <= 100; i++) {
+    output = '';
+    if (i % numOne === 0) {
+      output = stringOne;
+      } else if (i % numTwo === 0) {
+      output = stringTwo;
+      } else if (i % numOne !== 0 && i % numTwo !== 0) {
+      output = i;
+      }
+    console.log(output);
+    }
+};
+fizzBuzzCustom();
